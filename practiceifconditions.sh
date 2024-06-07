b912a91285586d50ca14d8fc36b2d5f5e88a5d94
mothukuru saikiran <obscuresai.com>
2024-06-07 14:06:03 +0530

    testing

---
+++ practiceifconditions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR= Please run with the root user"
+    exit 1
+else    
+    echo "Now you are root user"
+fi
+
+
+yum install GIT -y
+if [ $? -ne 0 ]
+then 
+    echo "ERROR= Git installation is failure,please run with the root user"
+    exit 1
+else
+    echo "Installation of GIT is success"
+    exit 0
+fi

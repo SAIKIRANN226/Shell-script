71145a4f741fb66cc85e35f11cdccc5a62d43ef6
mothukuru saikiran <obscuresai.com>
2024-06-08 20:08:45 +0530

    testing

--- practice.sh
+++ practice.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-ID=$(id -u)
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then 
-        echo "ERROR= $2 ...failed"
-        exit 1
-    else
-        echo "$2 ...sucess"
-}
-
+ID=(id -u)
 if [ $ID -ne 0 ]
 then 
-    echo "ERROR= not root user"
+    echo "ERROR= you are not root user"
     exit 1
 else
-    echo "root user"
+    echo "Now you are root user"
 fi
 
 yum install mysql -y
-
-VALIDATE $? "Installing mysql"
+if [ $? -ne 0 ]
+then 
+    echo "Installing mysql is $R failed you are not root user Please get root access and try"
+    exit 1
+else
+    echo "Installing mysql is $Y SUCCESS"
+fi
 
 yum install git -y
-
-VALIDATE $? "Installing git"
+if [ $? -ne 0 ]
+then 
+    echo "Installing git is $R failed you are not root user Please get root access and try"
+    exit 1
+else
+    echo "Installing git is $Y SUCCESS"
+fi

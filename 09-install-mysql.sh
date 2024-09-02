93b9a731f7da6bf420f6fc3d7ad46833baa40e41
mothukuru saikiran <obscuresai.com>
2024-09-02 19:34:47 +0530

    message

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -12,7 +12,7 @@ fi # fi means reverse of if, indicating condition end
 
 yum install mysql -y # -y should give otherwise program will wait for our input
 
-if [ $? -ne 0 ]
+if [ $? -ne 0 ]   # the only disadvantage in shell is it wont stop if it is facing any error so it is our responsibility to check,so to stop and slove the error first thats y we are using this exit status| $? ---> if success it has 0 ; if failure not 0
 then
     echo "ERROR:: Installing MySQL is failed"
     exit 1

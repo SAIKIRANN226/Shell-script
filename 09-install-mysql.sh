0231b8e2b2b0ccc8d73c4a8eb65d74fadff6b3bc
mothukuru saikiran <obscuresai.com>
2024-09-02 19:05:56 +0530

    message

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -10,7 +10,7 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-yum install mysql -y
+yum install mysql -y # -y should give otherwise program will wait for our input
 
 if [ $? -ne 0 ]
 then

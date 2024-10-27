fa099885033e4bde67479f9198ebbfa7e3b0e6c6
mothukuru saikiran <obscuresai.com>
2024-10-27 17:32:30 +0530

    message

--- 20-practice.sh
+++ 20-practice.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
 ID=$(id -u)
-DATE=$(date)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGFILE="/tmp/$0/saikiran.log
-
-VALIDATE(){
-     if [ $1 -ne 0 ]
-     then
-        echo -e "$2 ERROR: $R... failed $N"
-     else
-        echo -e "$2 ...$G SUCCESS $N"
-    fi
-}
-
-
 if [ $ID -ne 0 ]
-then
-    echo -e "$R ERROR: Please run this script with the root user"
+then 
+    echo -e "$R ERROR:: Please run this script with root user"
+    exit 1
 else
-    echo -e "$Y Now you are root user"
+    echo -e "$Y Now you are root user Please proceed with the script"
 fi
 
-yum install mysql -y 
-VALIDATE $? "Installing mysql"
+yum install mysql -y
 
+if [ $? -ne 0 ]
+then 
+    echo -e "$R Installing mysql is FAILED"
+    exit 1
+else
+    echo -e "$G Installing mysql is SUCCESS"
+fi

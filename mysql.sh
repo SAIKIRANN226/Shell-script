30a32dc8b633c9378cd7a12b20e6984d1b940fc1
mothukuru saikiran <obscuresai.com>
2024-11-28 10:52:24 +0530

    messgae

--- mysql.sh
+++ mysql.sh
@@ -5,19 +5,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 DATE=$(date)
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then 
-        echo -e "$2.....$G FAILED $N"
-        exit 1
-    else
-        echo -e "$2.....$G SUCCESS $N"
-    fi 
-}
-
-
 
 if [ $ID -ne 0 ]
 then 
@@ -28,11 +17,11 @@ else
 fi 
 
 yum install mysql -y
-VALIDATE $? "Installing mysql"
 
-yum install git -y 
-VALIDATE $? "Installing git"
-
-yum install postfix -y
-VALIDATE $? "Installing postfix"
-
+if [ $? -ne 0 ]
+then 
+    echo -e "$R ERROR:: Installing mysql is failed $N"
+    exit 1
+else
+    echo -e "$G Installing mysql is SUCCESS $N"
+fi

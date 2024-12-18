6f4d9aced70030c4f0cfe84e033584017f013265
mothukuru saikiran <obscuresai.com>
2024-12-18 12:37:22 +0530

    message

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,5 +4,9 @@ DATE=$(date) # DATE --> variable ; $(date) --> value
 
 echo "Script started executing: ${DATE}" # You can also give like $DATE
 
+result=$(ls -la)
+
+echo "Files including hidden $result"
+
 
 # How do you run commands in shellscript and take the output ? "${variable-name}" here flower braces is mandatory

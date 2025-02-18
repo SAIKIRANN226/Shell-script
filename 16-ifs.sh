2b4c9bc68603aa793c76931df6f3bb3ae210f602
mothukuru saikiran <obscuresai.com>
2025-02-18 12:42:38 +0530

    shell

--- 16-ifs.sh
+++ 16-ifs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 file=/etc/passwd
 
 R="\e[31m"
@@ -11,11 +12,11 @@ then
     echo -e "$R Source directory: $file does not exists. $N"
 fi
 
-while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path # You can search in google "/etc/passwd format". we can keep anything as a internal field separator like comma,fullstop,slash etc.
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path # You can search in google "/etc/passwd format". We can keep anything as a internal field separator like comma,fullstop,slash etc.
 do
     echo "username: $username"
     echo "user ID: $user_id"
     echo "User Full name: $user_fullname"
 done < $file
 
-# If you enter "cat /etc/passwd" you will get all the user information which is not in good format so you need to read this properly by using while loop with Internal field separator(IFS)
+# If you enter "cat /etc/passwd" you will get all the user information which is not in good format, So you need to read this properly by using while loop with Internal field separator(IFS)

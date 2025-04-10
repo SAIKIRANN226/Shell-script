149bd6ea2c0d9b2af929a0e8ed821eb6ae0a6c36
mothukuru saikiran <obscuresai.com>
2025-04-10 17:10:06 +0530

    shell

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for i in {1..100000}
+for i in {1..1000}
 do
     echo "$i"
 done
+
+
+# Here "i" is not mandatory you can keep your name also, and only 2 dots are mandatory in flower braces between 2 values 1 and 1000

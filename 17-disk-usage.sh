26a9c7178763de941536ff3fb89d5fa3774fec5c
mothukuru saikiran <obscuresai.com>
2025-08-29 18:22:18 +0530

    testing

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -2,7 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File') # vE means, i dont want files with tmp and File | ---> Pipe symbol is "or" We have different types of file systems when you enter command "df -hT" but we want only xfs type because those are related to disks, so we do reverse search which is "grep -vE 'tmp|File'" Here v --> I dont want except files tmp and File and E --> Expressions
 DISK_THRESHOLD=1 # 1 is nothing but 1% as of now, we dint used 70% so we are keeping just 1% for practice
-message="" # Empty  
+message="" # Empty
 
 while IFS= read line
 do
@@ -19,10 +19,3 @@ echo -e "Message: $message"
 # echo "$message" | mail -s "High Disk Usage" info@joindevops.com, This line is from gmail.MD document, sometimes in our company we dont have access, linux team will configure mail.sh, so we can call them by using below command.
 
 sh mail.sh "DevOps Team" "High Disk Usage" "$message" "info@joindevops.com" "ALERT High Disk Usage"
-
-
-
-# First we are doing reverse search to find only disks because we have different file systems also using "df -hT | grep -vE 'tmp|File'"
-# Then DISK_THRESHOLD 
-# MESSAGE
-# We are giving disks information as input to the while loop
